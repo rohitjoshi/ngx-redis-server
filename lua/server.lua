6c4097a776288072a0b437004dd24e138d74d998
rohitjoshi <rohit.c.joshi@gmail.com>
2023-01-23 15:06:23 -0500

    replaced log level info to debug and warn to info

--- lua/server.lua
+++ lua/server.lua
@@ -24,13 +24,13 @@ local function dump(o)
    end
 end
 local function parse_cmd(packet, remaning_buffer)
-    ngx.log(ngx.INFO, "received command : ", packet)
+    ngx.log(ngx.DEBUG, "received command : ", packet)
     local consumed, args, typ = resp.decode( packet )
-    ngx.log(ngx.INFO, "consumed: ", dump(consumed))
-    ngx.log(ngx.INFO, "args: ", dump(args))
-    ngx.log(ngx.INFO, "typ: ", dump(typ))
+    ngx.log(ngx.DEBUG, "consumed: ", dump(consumed))
+    ngx.log(ngx.DEBUG, "args: ", dump(args))
+    ngx.log(ngx.DEBUG, "typ: ", dump(typ))
     local packet_len = string.len(packet)
-    ngx.log(ngx.INFO, "received command length: ", packet_len)
+    ngx.log(ngx.DEBUG, "received command length: ", packet_len)
             
     
     if not args then
@@ -39,11 +39,11 @@ local function parse_cmd(packet, remaning_buffer)
     end
     
     local cmd = string.upper(args[1])
-    ngx.log(ngx.INFO, "Command received: ", cmd)
+    ngx.log(ngx.DEBUG, "Command received: ", cmd)
 
     remaning_buffer = ""
     if consumed < packet_len then  
-      ngx.log(ngx.INFO, "Remaining buffer: ", packet_len - consumed)
+      ngx.log(ngx.DEBUG, "Remaining buffer: ", packet_len - consumed)
       remaning_buffer = string.sub(packet, consumed+1, packet_len)
     end
 
@@ -51,9 +51,9 @@ local function parse_cmd(packet, remaning_buffer)
 end
 
 local function process_cmd(sock, cmd, args)
-            ngx.log(ngx.INFO, "in process_cmd:", cmd)
+            ngx.log(ngx.DEBUG, "in process_cmd:", cmd)
             if args then 
-                ngx.log(ngx.INFO, "Total args:", table.getn(args))
+                ngx.log(ngx.DEBUG, "Total args:", table.getn(args))
             end
             if cmd == "QUIT" or cmd == "BYE" then
                 sock:send('+OK\r\n')
@@ -74,26 +74,26 @@ local function process_cmd(sock, cmd, args)
                 if auth_enabled then 
                     cache:set(authenticated_key, true)
                     local authenticated_val = cache:get(key)
-                    ngx.log(ngx.INFO, "Client authenticated_val from cache:", authenticated )
-                    ngx.log(ngx.INFO, "Authenticated successfully:", authenticated_val)
+                    ngx.log(ngx.DEBUG, "Client authenticated_val from cache:", authenticated )
+                    ngx.log(ngx.DEBUG, "Authenticated successfully:", authenticated_val)
                 end
-                ngx.log(ngx.WARN, "Command AUTH, Resp: +OK")
+                ngx.log(ngx.INFO, "Command AUTH, Resp: +OK")
                 return sock:send('+OK\r\n')
             elseif cmd == "PING" then 
                 if args[2] then 
                     local resp_msg  = '+"' .. tostring(args[2] .. '"\r\n')
-                    ngx.log(ngx.INFO, "Sending PING response with message:" , resp_msg)
-                    ngx.log(ngx.WARN, "Command PING, Resp:",  resp_msg)
+                    ngx.log(ngx.DEBUG, "Sending PING response with message:" , resp_msg)
+                    ngx.log(ngx.INFO, "Command PING, Resp:",  resp_msg)
                     sock:send(resp_msg)
                     return true
                 else 
-                   ngx.log(ngx.INFO, "Sending PONG")
-                   ngx.log(ngx.WARN, "Command PING, Resp:+PONG")
+                   ngx.log(ngx.DEBUG, "Sending PONG")
+                   ngx.log(ngx.INFO, "Command PING, Resp:+PONG")
                     sock:send('+PONG\r\n')
                     return true
                 end
             elseif cmd == "CONFIG" then 
-                ngx.log(ngx.WARN, "Command CONFIG, Resp:-ERR Not Supported")
+                ngx.log(ngx.INFO, "Command CONFIG, Resp:-ERR Not Supported")
                 sock:send('-ERR Not Supported\r\n')
                 return true
             end
@@ -101,9 +101,9 @@ local function process_cmd(sock, cmd, args)
             if auth_enabled then 
                 local authenticated_key = ngx.var.remote_addr .. ':' .. ngx.var.remote_port
                 local authenticated_val = cache:get(authenticated_key)
-                ngx.log(ngx.INFO, "Client authenticated:", authenticated_val )
+                ngx.log(ngx.DEBUG, "Client authenticated:", authenticated_val )
                 if not authenticated_val then 
-                    ngx.log(ngx.INFO, "Client not authenticated:", authenticated_key )
+                    ngx.log(ngx.DEBUG, "Client not authenticated:", authenticated_key )
                     sock:send("-ERR You must login first\r\n")
                     return true
                 end
@@ -120,7 +120,7 @@ local function process_cmd(sock, cmd, args)
                     return true
                  end 
                 local val_resp = '+' .. val .. '\r\n'
-                ngx.log(ngx.WARN, "Command GET: Resp:", val_resp)
+                ngx.log(ngx.INFO, "Command GET: Resp:", val_resp)
                 sock:send(val_resp)
                 return true
             elseif cmd == "SET"  then 
@@ -129,18 +129,18 @@ local function process_cmd(sock, cmd, args)
                     return true
                  end
                  cache:set(args[2], args[3])
-                ngx.log(ngx.WARN, "Command " .. cmd .. "Resp:+OK")
+                ngx.log(ngx.INFO, "Command " .. cmd .. "Resp:+OK")
                 sock:send('+OK\r\n')
                 return true
             elseif  cmd == "DEL" then 
                 cache:delete(args[2])
-                ngx.log(ngx.WARN, "Command " .. cmd .. "Resp:+OK")
+                ngx.log(ngx.INFO, "Command " .. cmd .. "Resp:+OK")
                 sock:send('+OK\r\n')
                 return true
             end
 
             if cmd == "HGET" then
-                ngx.log(ngx.INFO, "HGET command received")
+                ngx.log(ngx.DEBUG, "HGET command received")
                  if args[2] == nil or args[3] == nil  then
                     sock:send("-ERR must pass key and field\r\n")
                     return true
@@ -150,11 +150,11 @@ local function process_cmd(sock, cmd, args)
                     sock:send("-ERR not found\r\n")
                     return true
                  end
-                 ngx.log(ngx.INFO, "key: ", args[2])
-                 ngx.log(ngx.INFO, "field: ", args[3])
+                 ngx.log(ngx.DEBUG, "key: ", args[2])
+                 ngx.log(ngx.DEBUG, "field: ", args[3])
                  local val = key_table[args[3]] 
                  if not val then 
-                    ngx.log(ngx.INFO, "key: ", args[3], " not found")
+                    ngx.log(ngx.DEBUG, "key: ", args[3], " not found")
                     sock:send("-ERR not found\r\n")
                     return true
                  end
@@ -163,27 +163,27 @@ local function process_cmd(sock, cmd, args)
                  sock:send(msg)
                  return true
             elseif cmd == "HSET" then
-                ngx.log(ngx.INFO, "HSET command received")
+                ngx.log(ngx.DEBUG, "HSET command received")
                  if args[2] == nil or args[3] == nil or args[4] == nil then
                     sock:send("-ERR must pass key,  field and val \r\n")
                     return true
                  end
                  local key_table = cache:get(args[2])
                  if not key_table then 
-                    ngx.log(ngx.INFO, "Cache table not found for key: ", args[2])
+                    ngx.log(ngx.DEBUG, "Cache table not found for key: ", args[2])
                     key_table = {}
                  end
                  key_table[args[3]] = args[4]
 
                  cache:set(args[2], key_table)
                  
-                 ngx.log(ngx.INFO, "key: ", args[2])
-                 ngx.log(ngx.INFO, "field: ", args[3])
-                 ngx.log(ngx.INFO, "Val: ", args[4])
+                 ngx.log(ngx.DEBUG, "key: ", args[2])
+                 ngx.log(ngx.DEBUG, "field: ", args[3])
+                 ngx.log(ngx.DEBUG, "Val: ", args[4])
                  sock:send('+OK\r\n')
                  return true
             elseif cmd == "HMSET" then
-                ngx.log(ngx.INFO, "HSET command received")
+                ngx.log(ngx.DEBUG, "HSET command received")
                  if args[2] == nil or args[3] == nil or args[4] == nil then
                     sock:send("-ERR must pass key,  field and val \r\n")
                     return true
@@ -192,23 +192,23 @@ local function process_cmd(sock, cmd, args)
                  if not key_table then 
                     key_table = {}
                  end
-                  ngx.log(ngx.INFO, "key: ", args[2])
+                  ngx.log(ngx.DEBUG, "key: ", args[2])
                  for i, v in ipairs(args) do
                     if i > 2 and i % 2 > 0 then 
                         key_table[args[i]] = args[i+1]
-                        ngx.log(ngx.INFO, "key: ", args[i], "value:", args[i+1])
+                        ngx.log(ngx.DEBUG, "key: ", args[i], "value:", args[i+1])
                     end
                   end
                  sock:send('+OK\r\n')
                  return true
             elseif cmd == "HMGET" then
-                ngx.log(ngx.INFO, "HMGET command received")
+                ngx.log(ngx.DEBUG, "HMGET command received")
                  if args[2] == nil or args[3] == nil  then
                     sock:send("-ERR must pass key and atleast field\r\n")
                     return true
                  end
-                 ngx.log(ngx.INFO, "key: ", args[2])
-                 ngx.log(ngx.INFO, "field: ", args[3])
+                 ngx.log(ngx.DEBUG, "key: ", args[2])
+                 ngx.log(ngx.DEBUG, "field: ", args[3])
                  local key_table = cache:get(args[2])
                  if not key_table then 
                     sock:send("-ERR not found\r\n")
@@ -237,11 +237,11 @@ end
 function M.run()
     local sock = assert(ngx.req.socket(true))
     sock:settimeout(60000)  -- one second timeout
-    ngx.log(ngx.WARN, "**Connection received from:", tostring(ngx.var.remote_addr) .. ":" .. tostring(ngx.var.remote_port))
+    ngx.log(ngx.INFO, "**Connection received from:", tostring(ngx.var.remote_addr) .. ":" .. tostring(ngx.var.remote_port))
             
-    ngx.log(ngx.INFO, "$connection: ", tostring(ngx.var.connection))
-    ngx.log(ngx.INFO, "$remote_addr: ", tostring(ngx.var.remote_addr))
-    ngx.log(ngx.INFO, "$remote_port: ", tostring(ngx.var.remote_port))
+    ngx.log(ngx.DEBUG, "$connection: ", tostring(ngx.var.connection))
+    ngx.log(ngx.DEBUG, "$remote_addr: ", tostring(ngx.var.remote_addr))
+    ngx.log(ngx.DEBUG, "$remote_port: ", tostring(ngx.var.remote_port))
     local buffer = ""
     while true do
         
@@ -250,41 +250,41 @@ function M.run()
          sock:settimeout(60000)  -- one second timeout
           packet, err = sock:receiveany(16384)
           if not packet or err then
-            ngx.log(ngx.WARN, "**Connection closed from:", tostring(ngx.var.remote_addr) .. ":" .. tostring(ngx.var.remote_port) .. ". Reason:", err)
+            ngx.log(ngx.INFO, "**Connection closed from:", tostring(ngx.var.remote_addr) .. ":" .. tostring(ngx.var.remote_port) .. ". Reason:", err)
     
-            --ngx.log(ngx.WARN, "failed to read packet content: ", err)
+            --ngx.log(ngx.INFO, "failed to read packet content: ", err)
             sock:send("failed to read packet content: " .. err)
             return false
           end
            -- local packet = tostring(packet)
-          --ngx.log(ngx.INFO, "received command: ", packet)
+          --ngx.log(ngx.DEBUG, "received command: ", packet)
 
           local packet = buffer .. packet
           buffer = ""
           local cmd, args, consumed, buffer = parse_cmd(packet, buffer)
           if not process_cmd(sock, cmd, args) then
               ngx.flush(true)
-              ngx.log(ngx.INFO, "Exiting loop");
+              ngx.log(ngx.DEBUG, "Exiting loop");
              return
          end
           --ngx.flush(true)
-          ngx.log(ngx.INFO, "consumed:", consumed);
-           ngx.log(ngx.INFO, "remaning buffer:", buffer);
-          ngx.log(ngx.INFO, "remaning buffer length:", string.len(buffer));
+          ngx.log(ngx.DEBUG, "consumed:", consumed);
+           ngx.log(ngx.DEBUG, "remaning buffer:", buffer);
+          ngx.log(ngx.DEBUG, "remaning buffer length:", string.len(buffer));
           
          while consumed > 0 and string.len(buffer) > 0 do 
-            ngx.log(ngx.INFO, "Still ");
+            ngx.log(ngx.DEBUG, "Still ");
             local remaning_buffer = buffer
             buffer = ""
             cmd, args, consumed, buffer = parse_cmd(remaning_buffer, buffer)
             if not process_cmd(sock, cmd, args) then
               ngx.flush(true)
-              ngx.log(ngx.INFO, "Exiting loop");
+              ngx.log(ngx.DEBUG, "Exiting loop");
              return
             end 
-            ngx.log(ngx.INFO, "consumed:", consumed);
-             ngx.log(ngx.INFO, "remaning buffer:", buffer);
-            ngx.log(ngx.INFO, "remaning buffer length:", string.len(buffer));
+            ngx.log(ngx.DEBUG, "consumed:", consumed);
+             ngx.log(ngx.DEBUG, "remaning buffer:", buffer);
+            ngx.log(ngx.DEBUG, "remaning buffer length:", string.len(buffer));
         end
         
        
